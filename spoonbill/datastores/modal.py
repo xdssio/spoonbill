4b28f6bd98307aeec2b33625f9a27b5a493deac1
xdssio <jonathan@xdss.io>
2023-01-11 15:10:30 +0100

    adjust tests

--- spoonbill/datastores/modal.py
+++ spoonbill/datastores/modal.py
@@ -1,5 +1,4 @@
-import contextlib
-from spoonbill.datastores import KeyValueStore, VALUE
+from spoonbill.datastores import KeyValueStore, VALUE, KEY
 import modal
 
 
@@ -9,43 +8,53 @@ class ModalStore(KeyValueStore):
 
     def __init__(self, stub: str, data={}):
         self.stub = stub
-        self.stub.main = modal.Dict(data=data)
-        self.stub.metadata = modal.Dict(data={ModalStore.KEYS: set(data.keys()), ModalStore.SIZE: len(data)})
+        self.stub._main = modal.Dict(data=data)
+        self.stub._metadata = modal.Dict(data={ModalStore.KEYS: set(data.keys()), ModalStore.SIZE: len(data)})
         self._store = None
+
+    def _contains(self, key):
+        """
+        Check if the key is in the modal dict.
+        :param key:
+        :return:
+        """
+        return key in self._store._main
 
     @property
     def _keys(self):
-        return self._store.metadata[ModalStore.KEYS]
+        return self._store._metadata[ModalStore.KEYS]
+
+    def _iter_keys(self):
+        for key in self._keys:
+            if self._contains(key):
+                yield key
 
     @property
     def _size(self):
-        return self._store.metadata[ModalStore.SIZE]
+        return self._store._metadata[ModalStore.SIZE]
 
     def _add_key(self, key):
-        if key not in self._store.main:
+        if not self._contains(key):
             self._increment_size()
-        self._store.metadata.put(ModalStore.KEYS, self._keys.union([key]))
+        self._store._metadata.put(ModalStore.KEYS, self._keys.union([key]))
 
     def _remove_key(self, key):
-        if key in self._keys:
+        if self._contains(key):
             self._decrement_size()
-        self._store.metadata.put(ModalStore.KEYS, self._keys.difference([key]))
+        self._store._metadata.put(ModalStore.KEYS, self._keys.difference([key]))
 
     def _increment_size(self):
-        self._store.metadata.put(ModalStore.SIZE, self._size + 1)
+        self._store._metadata.put(ModalStore.SIZE, self._size + 1)
 
     def _decrement_size(self):
-        self._store.metadata.put(ModalStore.SIZE, self._size - 1)
+        self._store._metadata.put(ModalStore.SIZE, self._size - 1)
 
-    def keys(self):
-        return iter(self._store._keys)
-
-    def set_store(self, store):
-        self._store = store
+    def set_context(self, app):
+        self._store = app
 
     def _setitem(self, key, value):
-        self._store.main[key] = value
         self._add_key(key)
+        self._store._main.put(key, value)
 
     def __setitem__(self, key, value):
         self._setitem(key, value)
@@ -54,8 +63,8 @@ class ModalStore(KeyValueStore):
         self._setitem(key, value)
 
     def _getitem(self, key, default=None):
-        if key in self._store.keys[ModalStore.KEYS]:
-            return self._store.main[key]
+        if self._contains(key):
+            return self._store._main.get(key)
         return default
 
     def __getitem__(self, key):
@@ -68,8 +77,9 @@ class ModalStore(KeyValueStore):
         return self._getitem(key, default)
 
     def _delitem(self, key):
-        self._store.main.pop(key)
+        value = self._store._main.pop(key)
         self._remove_key(key)
+        return value
 
     def __delitem__(self, key):
         self._delitem(key)
@@ -77,17 +87,19 @@ class ModalStore(KeyValueStore):
     def __len__(self):
         return self._size
 
-    def keys(self):
-        for key in self._keys:
-            if key not in self._store.main:
-                continue
-            yield key
+    def keys(self, pattern: str = None, limit: int = None, *args, **kwargs):
+        is_valid = self._to_filter(KEY, pattern) if pattern else lambda x: True
+        for i, key in enumerate(self._iter_keys()):
+            if i == limit:
+                break
+            if is_valid(key):
+                yield key
 
     def _items(self):
         for key in self._keys:
-            if key not in self._store.main:
+            if key not in self._store._main:
                 continue
-            yield key, self._store.main[key]
+            yield key, self._store._main.get(key)
 
     def items(self, conditions: dict = None, limit: int = None):
         if conditions is not None and not hasattr(conditions, 'items'):
@@ -105,7 +117,15 @@ class ModalStore(KeyValueStore):
             for i, key in enumerate(self._keys):
                 if i == limit:
                     break
-                yield self._store.main[key]
+                yield self.get(key, default)
+
+    def update(self, data: dict):
+        for key, value in data.items():
+            self[key] = value
+
+    def _flush(self):
+        for key in self._keys:
+            self._delitem(key)
 
     def encode_key(self, key):
         return key
@@ -118,3 +138,8 @@ class ModalStore(KeyValueStore):
 
     def decode_value(self, value):
         return value
+
+    def pop(self, key, default=None):
+        if not self._contains(key):
+            return default
+        return self._delitem(key)
--- tests/modal_test.py
+++ tests/modal_test.py
@@ -4,8 +4,7 @@ import modal
 
 image = modal.Image.debian_slim().pip_install("spoonbill-framework")
 stub = modal.Stub("example-hello-world")
-store = ModalStore(stub, data={"hello": "world"})
-stub.main = modal.Dict(data={"s": set([1, 2])})
+store = ModalStore(stub)
 
 
 @stub.function(image=image)
@@ -20,20 +19,31 @@ def f(i):
 
 if __name__ == "__main__":
     with stub.run() as app:
-        store.set_store(app)
-        print(len(store), store._keys)
-        for i in range(3):
-            store[i] = f(i)
-        print(store)
-        # Call the function directly.
-        # store.set_store(app)
-        # key = 4
-        # store[key] = f.call(key)
-        # print(store)
+        store.set_context(app)
+        store._flush()
+        store['test'] = 'test'
+        assert len(store) == 1
+        assert store['test'] == store.get('test') == 'test'
+        store.set('another', 'another')
+        assert 'test' in store  # test contains
 
-        # Parallel map.
-        # total = 0
-        # for ret in f.map(range(10)):
-        #     total += ret
-        #
-        # print(total)
+        assert set(store.keys()) == set(['test', 'another'])
+        assert set(store.values()) == set(['test', 'another'])
+        assert set(store.items()) == set([('test', 'test'), ('another', 'another')])
+
+        assert list(store.keys(pattern='test')) == ['test']
+        assert list(store.values(keys=['test'])) == ['test']
+        assert list(store.items(conditions='test')) == [('test', 'test')]
+
+        assert store.pop('another') == 'another'
+        print(store)
+        assert len(store) == 1
+
+        assert store.update({'test': 'test2', 'another': 'another2'})
+        assert len(store) == 2
+        assert store == {'test': 'test2', 'another': 'another2'}
+
+        store['test'] = {'a': 1, 'b': 2}
+        assert list(store.items(conditions={'a': 1})) == [('test', {'a': 1, 'b': 2})]
+        store['function'] = lambda x: x + 1
+        assert store['function'](1) == 2
